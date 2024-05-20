9ebd2b8e5db84715193386ceb29b63983031adfc
monaeeee <147624231+monaeeee@users.noreply.github.com>
2024-05-20 13:16:34 +0000

    I finished my project

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+let button = document.querySelector("button");
+let results = document.querySelector(".r");
+let source= document.querySelector(".source");
+let video= document.querySelector(".video");
+
+button.addEventListener("click", function(){
+   let age = document.querySelector(".inputone").value;
+   let album = document.querySelector(".input2").value;
+
+   
+   if (12 < age < 26 && album === 'Ctrl'){ 
+   results.innerHTML = "You're Normal girl by sza";
+    source="https://youtu.be/WxflcXmtVTM?si=vYjeZ44MYTJxyNlv";
+     } else if (age >=27 && album === "Sos"){
+   results.innerHTML = "You're The Weekend by sza";
+   } else if(12 < age < 26 && album === 'Sos'){ 
+    results.innerHTML = "You're Nobdy gets me by sza";
+    source="https://youtu.be/3mQ5Yaehtpg?si=k5q5rpYxABnbdmws";
+   } else if (age >=27 && album === 'Ctrl') {
+     results.innerHTML = "You're Snooze by sza";
+   } else {
+     results.innerHTML="Your Results do not match any song"
+   };
+
+   // if (12 < age < 26 && album === 'Sos'){ 
+   //    results.innerHTML = "You're Nobdy gets me by sza";
+   //     source="https://youtu.be/3mQ5Yaehtpg?si=k5q5rpYxABnbdmws";
+   //      } else if (age >=27 && album === "Ctrl"){
+   //    results.innerHTML = "You're Snooze by sza";
+   //    }else{results.innerHTML="Your Results do not match any song"};
+
+
+    });
+
+
+
+
+
+
+
+
+
 // JavaScript goes here.
